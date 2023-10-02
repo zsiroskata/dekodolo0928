bcaeeaf89eb59d07cfc06d89844a77779e4be91a
ny19zsírosk <ny19zsrosk@203-7>
2023-10-02 09:12:27 +0200

    Add project files.

---
+++ dekodolo0928/Karakter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dekodolo0928
+{
+    class Karakter
+    {
+        public char Betu { get; set; }
+        public bool[,] Matrix { get; set; }
+
+        public Karakter(char betu, bool[,] matrix)
+        {
+            Betu = betu;
+            Matrix = matrix;
+        }
+    }
+}
---
+++ dekodolo0928/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace dekodolo0928
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var karakter = new List<Karakter>();
+            var sr = new StreamReader(
+                path:@"..\..\..\dekodol.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while (!sr.EndOfStream)
+            {
+                char b = char.Parse(sr.ReadLine());
+                bool[,] m = new bool[7, 4];
+
+                for (int s = 0; s < 7; s++)
+                {
+                    string sor = sr.ReadLine();
+                    for (int o = 0; o < sor.Length; o++)
+                    {
+                        m[s, o] = sor[o] == '1';
+                    }
+                }
+                karakter.Add(new Karakter(b, m));
+            }
+            Console.WriteLine($"karakter szam {karakter.Count}");
+            char input = ' ';
+            bool res = false;
+            do
+            {
+                Console.WriteLine("input: ");
+                //input = char.Parse(Console.ReadLine());
+                res = char.TryParse(Console.ReadLine(), out input);
+            }while (!res || input < 65 || input > 90) ;
+
+            //ascii table 
+        }
+    }
+}
